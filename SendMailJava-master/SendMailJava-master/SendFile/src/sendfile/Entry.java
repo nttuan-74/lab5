35cd2508a47410009804ff23552913f1a86fab17
nttuan-74 <113812969+nttuan-74@users.noreply.github.com>
2022-09-25 21:54:21 +0700

    Add files via upload

---
+++ SendMailJava-master/SendMailJava-master/SendFile/src/sendfile/Entry.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sendfile;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Entry {
+    public static void main(String[] args) {
+        Action action = new Action();
+        action.getGui().setVisible(true);
+    }
+}
